11fff3b7c055cbbf3c70a8fe433a13b368593715
nikb15 <nikki.bhujbal2011@gmail.com>
2022-11-15 20:43:55 +0530

    2.3

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,3 +1,7 @@
 from . import Expense 
 expenses =Expense.Expenses()
 expenses.read_expenses("data/spending_data.csv")
+spending_categories={}
+
+for expenses in expenses.list:
+    spending_categories.append(expenses)
